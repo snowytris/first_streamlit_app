687a00961c767c46234ca01a5a1250c0facc16f1
snowytris <128911137+snowytris@users.noreply.github.com>
2023-03-28 18:13:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,15 @@
 
-import snowflake.connector
+from snowflake.connector import connect
 import streamlit as st
 
+
+conn = connect(**st.secrets["snowflake"])
+cursor = conn.cursor()
+
+# Execute some SQL in the warehouse
+cursor.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION();")
+# Fetch the results
+one_row = cursor.fetchone()
+
 st.title("Coucou")
+st.text(one_row)
