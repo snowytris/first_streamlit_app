b2573e4cf3931091be9518f875f6cbf2f47a770e
snowytris <128911137+snowytris@users.noreply.github.com>
2023-03-29 10:56:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,3 +13,22 @@ one_row = cursor.fetchone()
 
 st.title("Coucou")
 st.text(one_row)
+
+
+fruit_to_add = st.text_input("Ajouter un fruit")
+add_fruit = st.button("Add fruit!")
+
+if add_fruit:
+  with conn.cursor() as c:
+    c.execute(f"""
+    INSERT INTO fruit_load_list VALUES 
+    ('{fruit_to_add}');
+    """)
+
+  # Display all fruits
+  
+ with conn.cursor() as c:
+  all_fruits = c.execute("SELECT * FROM fruit_load_list;").fetchall()
+  st.dataframe(all_fruits)
+  
+  
